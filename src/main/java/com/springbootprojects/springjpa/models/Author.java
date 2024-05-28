5ac154c153b7ec16ccb903da35d700faf75e9f87
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-05-27 22:05:46 -0500

    feat: add many to many relationship author-course

--- src/main/java/com/springbootprojects/springjpa/models/Author.java
+++ src/main/java/com/springbootprojects/springjpa/models/Author.java
@@ -1,10 +1,13 @@
 package com.springbootprojects.springjpa.models;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -52,4 +55,9 @@ public class Author {
     nullable = false
     )
     private int age;
+    
+    @ManyToMany(
+        mappedBy = "authors"
+    )
+    private List<Course> courses;
 }
--- src/main/java/com/springbootprojects/springjpa/models/Course.java
+++ src/main/java/com/springbootprojects/springjpa/models/Course.java
@@ -1,10 +1,15 @@
 package com.springbootprojects.springjpa.models;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -38,4 +43,16 @@ public class Course {
     length = 200
     )
     private String description;
+
+    @ManyToMany
+    @JoinTable(
+        name = "authors_courses",
+        joinColumns = {
+            @JoinColumn(name = "course_id")
+        },
+        inverseJoinColumns = {
+            @JoinColumn(name = "author_id")
+        }
+    )
+    private List<Author> authors;
 }
