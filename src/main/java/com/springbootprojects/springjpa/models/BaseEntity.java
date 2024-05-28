f7b1fc526b604141c1e511a7f2bda740f1d4945d
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-05-28 14:24:01 -0500

    feat: create superclass to inherit common props

---
+++ src/main/java/com/springbootprojects/springjpa/models/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.springbootprojects.springjpa.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(
+        strategy = GenerationType.AUTO
+    )
+    private Integer id;
+
+    @Column( nullable = false )
+    private LocalDateTime createdAt;
+
+    @Column( nullable = false )
+    private LocalDateTime latModifiedAt;
+
+    @Column( nullable = false )
+    private String createdBy;
+
+    @Column( nullable = false )
+    private String lastModifiedBy;
+}
