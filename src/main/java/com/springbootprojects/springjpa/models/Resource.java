0cae473f1d174786a1e08ecc21b6d1471682e075
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-05-27 21:37:30 -0500

    feat: create resource entity

---
+++ src/main/java/com/springbootprojects/springjpa/models/Resource.java
@@ -0,0 +1,47 @@
+package com.springbootprojects.springjpa.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "resources")
+public class Resource {
+
+    @Id
+    @GeneratedValue(
+        strategy = GenerationType.AUTO
+    )
+    private Integer id;
+
+    @Column(
+    name = "resource_name",
+    nullable = false,
+    length = 50
+    )
+    private String name;
+
+    @Column(
+    name = "lecture_size",
+    nullable = false
+    )
+    private int size;
+
+    @Column(
+    name = "lecture_url",
+    nullable = false,
+    length = 100
+    )
+    private String url;
+}
