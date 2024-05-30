1bd8b4c7cba36133df44709faa93777c3c975207
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-05-30 16:29:38 -0500

    feat: create derivative query methods for Author

--- src/main/java/com/springbootprojects/springjpa/repositories/AuthorRepository.java
+++ src/main/java/com/springbootprojects/springjpa/repositories/AuthorRepository.java
@@ -3,7 +3,31 @@ package com.springbootprojects.springjpa.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springbootprojects.springjpa.models.Author;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
 
+    List<Author> findAllByFirstName(String firstName);
+
+    List<Author> findAllByFirstNameIgnoreCase(String firstName);
+
+    List<Author> findAllByFirstNameContainingIgnoreCase(String firstName);
+
+    List<Author> findAllByFirstNameStartsWithIgnoreCase(String firstName);
+
+    List<Author> findAllByFirstNameEndsWithIgnoreCase(String firstName);
+
+    List<Author> findAllByFirstNameInIgnoreCase(List<String> firstNames);
+
+    void deleteAllByAge(int age);
+
+    List<Author> findAllByFirstNameAndLastName(String firstName, String lastName);
+
+    int countAllByAge(int age);
+
+    List<Author> findAllByAgeNull(int age);
+
+    Optional<Author> findFirstByLastName(String lastName);
 }
