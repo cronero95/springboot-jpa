af2560c4e04ebb8ebdbbea591f36268d89be7a34
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-05-31 21:57:50 -0500

    feat: create two predicates to query "authors" inf

---
+++ src/main/java/com/springbootprojects/springjpa/specifications/AuthorSpecification.java
@@ -0,0 +1,36 @@
+package com.springbootprojects.springjpa.specifications;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.springbootprojects.springjpa.models.Author;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+public class AuthorSpecification {
+
+    public static Specification<Author> hasAge(int age) {
+        return(
+            Root<Author> root, 
+            CriteriaQuery<?> query, 
+            CriteriaBuilder builder
+        ) -> {
+            if(age < 0) return null;
+
+            return builder.equal(root.get("age"), age);
+        };
+    }
+
+    public static Specification<Author> firstNameLike(String firstName) {
+        return(
+            Root<Author> root, 
+            CriteriaQuery<?> query, 
+            CriteriaBuilder builder
+        ) -> {
+            if(firstName == null) return null;
+
+            return builder.like(root.get("firstName"), firstName);
+        };
+    }
+}
